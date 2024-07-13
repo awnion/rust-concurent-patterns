4d7a5ce7a970543ac17535525a542262db04fec3
Sergei Blinov <blinovsv@gmail.com>
2024-07-13 13:37:51 +0200

    channels

--- src/bin/futures.rs
+++ src/bin/futures.rs
@@ -1,3 +1,4 @@
+use std::any::type_name_of_val;
 use std::thread::sleep;
 
 use rust_concurent_patterns::f::arc_atomic_counter;
@@ -6,18 +7,25 @@ use rust_concurent_patterns::f::std_sync_mpsc_channel;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    println!("------------");
+    sleep(std::time::Duration::from_secs(1));
+    println!("------------");
+
+    println!("{}", type_name_of_val(&std_sync_mpsc_channel));
     std_sync_mpsc_channel();
 
     println!("------------");
     sleep(std::time::Duration::from_secs(1));
     println!("------------");
 
+    println!("{}", type_name_of_val(&crossbeam_unbounded));
     crossbeam_unbounded();
 
     println!("------------");
     sleep(std::time::Duration::from_secs(1));
     println!("------------");
 
+    println!("{}", type_name_of_val(&arc_atomic_counter));
     arc_atomic_counter();
 
     Ok(())
